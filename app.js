05d7309868326ef0d64069b38217d9b2a99e79aa
Tahori <36399680+giro0710@users.noreply.github.com>
2019-01-07 23:26:46 +0800

    persistent menu fixed

--- app.js
+++ app.js
@@ -432,6 +432,7 @@ app.post('/webhook', (req, res) => {
 	let body = req.body;
 
 	if (body.object === 'page') {
+		console.log(JSON.stringify(body.entry));
 		body.entry.forEach(function(entry) {
 
 			let webhook_event = entry.messaging[0];
